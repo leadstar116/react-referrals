2efa729faa47fabc8ad13917128cebfc49f7ffcc
leadstar116 <supremedev116@gmail.com>
2020-07-05 05:39:37 +1000

    add user actions

---
+++ src/_actions/user.actions.ts
@@ -0,0 +1,25 @@
+import { UserData } from '../_constants/user.interface'
+
+export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS'
+export const userLogInSuccess = (user: UserData) => ({
+    type: USER_LOGIN_SUCCESS,
+    payload: { user }
+})
+
+export const USER_LOGIN_FAILURE = 'USER_LOGIN_FAILURE'
+export const userLogInFailure = (error: string) => ({
+    type: USER_LOGIN_SUCCESS,
+    payload: { error }
+})
+
+export const USER_REGISTER_SUCCESS = 'USER_REGISTER_SUCCESS'
+export const userRegisterSuccess = (user: UserData) => ({
+    type: USER_REGISTER_SUCCESS,
+    payload: { user }
+})
+
+export const USER_REGISTER_FAILURE = 'USER_REGISTER_FAILURE'
+export const userRegisterFailure = (error: string) => ({
+    type: USER_REGISTER_FAILURE,
+    payload: { error }
+})
